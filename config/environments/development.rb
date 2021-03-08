346714bb806bc74b9648739a91107f943816439a
Abhishek Pandey <abhishek.pandey@bluebash.co>
2021-03-08 17:54:39 +0530

    Styling Update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,8 +60,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-
-  # Raises error for missing translations.
+   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
   # Annotate rendered view with file names.
@@ -74,4 +73,14 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                  587,
+    domain:               'localhost:3000',
+    user_name:            'ap2866925@gmail.com',
+    password:             'Bluebash@1234',
+    authentication:       'plain',
+    enable_starttls_auto: true }
+ 
 end
