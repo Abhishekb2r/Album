70e021f384a53b3e7d8c10d289e5c0ca760c0016
Abhishek Pandey <abhishek.pandey@bluebash.co>
2021-03-12 14:46:11 +0530

    production update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
+  config.action_mailer.default_url_options = { host: 'railsalbums.herokuapp.com', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                  587,
+    domain:               'railsalbums.herokuapp.com',
+    user_name:            'ap2866925@gmail.com',
+    password:             'Bluebash@1234',
+    authentication:       'plain',
+    enable_starttls_auto: true }
+ 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
